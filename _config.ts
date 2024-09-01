89e362b4666b135bc9e71de7b9cdad4cc528c642
Thiago 'Jedi' Cerqueira <thiagoa7@gmail.com>
2024-09-01 00:41:35 -0300

    Deploy para o github pages

--- _config.ts
+++ _config.ts
@@ -1,7 +1,9 @@
 import lume from "lume/mod.ts";
 import jsx_preact from "lume/plugins/jsx_preact.ts";
 
-const site = lume();
+const site = lume({
+    location: new URL("https://thiagojedi.github.io/fedisaudades")
+});
 
 site.use(jsx_preact());
 
